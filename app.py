77bf3920492c07c50c8a14f130dec1d943eb14e2
R Yaishnave Devi <ryaishnavedevi@gmail.com>
2026-07-10 19:53:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import google.generativeai as genai
+
+import streamlit as st
+import google.generativeai as genai
+
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+# Configure Gemini API
+#genai.configure(api_key="AQ.Ab8RN6IxxmuY9IgkIjWlYXdWkk3nbH5-Q93qPfDwCLsX_AnP0Q")
+model = genai.GenerativeModel("gemini-2.5-flash")
+st.set_page_config(page_title="AI Learning Buddy-Yaishnave", page_icon="🎓🎓")
+st.title("🎓🎓 AI Learning Buddy-Yaishnave")
+topic = st.text_input("Enter a Topic")
+option = st.selectbox(
+"Choose Activity",
+[
+"Explain Concept",
+"Real-Life Example",
+"Generate Quiz",
+"Ask Anything"
+]
+)
+if st.button("Generate"):
+  if topic == "":
+    st.warning("Please enter a topic.")
+  else:
+    if option == "Explain Concept":
+      prompt = f"Explain {topic} in simple language for a beginner."
+    elif option == "Real-Life Example":
+      prompt = f"Give one simple real-life example of {topic}."
+    elif option == "Generate Quiz":
+      prompt = f"Create 5 MCQs on {topic} with answers."
+    else:
+      prompt = topic
+  response = model.generate_content(prompt)
+  st.write(response.text)
